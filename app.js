a713aa929852f40f42708244a61da6b00894c3e5
Varshitha-11 <137818105+Varshitha-11@users.noreply.github.com>
2023-11-08 18:58:16 -0600

    use a good message

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var Devices = require("./models/Devices");
 
 var app = express();
 
@@ -18,6 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +46,47 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+// We can seed the collection if needed onserver start
+async function recreateDB(){
+ // Delete everything
+//  await Devices.deleteMany();
+ let instance1 = new
+Devices({name:"Laptop", color:'Pink',
+ram:250});
+ instance1.save().then(doc=>{
+ console.log("First object saved")}
+ ).catch(err=>{
+ console.error(err)
+ });
+
+
+let instance2 = new
+Devices({name:"Tv", color:'Bule',
+ram:128});
+ instance2.save().then(doc=>{
+ console.log("secound object saved")}
+ ).catch(err=>{
+ console.error(err)
+ });
+
+ let instance3 = new
+Devices({name:"Mobile", color:'Red',
+ram:250});
+ instance3.save().then(doc=>{
+ console.log("Thired object saved")}
+ ).catch(err=>{
+ console.error(err)
+ });
+}
+let reseed = true;
+if (reseed) {recreateDB();}
 
 module.exports = app;
---
+++ models/Devices.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const DeviceSchema = mongoose.Schema({
+name: String,
+color: String,
+ram: Number
+})
+module.exports = mongoose.model("Device",
+DeviceSchema)
