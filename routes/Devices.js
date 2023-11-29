be454a50b5c6c73474c0420f9a610ff5d89c800a
Varshitha-11 <137818105+Varshitha-11@users.noreply.github.com>
2023-11-29 11:11:17 -0600

    done

--- routes/Devices.js
+++ routes/Devices.js
@@ -16,10 +16,10 @@ router.get('/Devices/:id', Device_controlers.Device_detail)
 /* GET detail Devicespage */
 router.get('/detail', Device_controlers.Device_view_one_Page);
 /* GET create Devicespage */
-router.get('/create', Device_controlers.Device_create_Page);
+router.get('/create', secured, Device_controlers.Device_create_Page);
 /* GET create update page */
-router.get('/update', Device_controlers.Device_update_Page);
+router.get('/update', secured,Device_controlers.Device_update_Page);
 /* GET delete Devicespage */
-router.get('/delete', Device_controlers.Device_delete_Page);
+router.get('/delete', secured,Device_controlers.Device_delete_Page);
 
 module.exports = router;
